8c6bbdc8e182ae41232a59b922493c98817bbdec
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-30 16:29:41 +0100

    Let queen and bishop take opposing pieces

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -31,7 +31,11 @@ namespace Chessington.GameEngine.Pieces
                     var newCol = currentSquare.Col + i * colDir;
                     
                     if (newRow < 0 || newRow >= boardSize || newCol < 0 || newCol >= boardSize) break;
-                    availableMoves.Add(Square.At(newRow, newCol));
+                    
+                    if (board.GetPiece(Square.At(newRow, newCol)) == null || board.GetPiece(Square.At(newRow, newCol)).Player != this.Player)
+                    {
+                        availableMoves.Add(Square.At(newRow, newCol));
+                    }
                     if (board.GetPiece(Square.At(newRow, newCol)) != null) break;
                 }
             }
