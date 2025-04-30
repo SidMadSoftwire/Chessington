3301a50066e290d261f268528c79f400a69987ed
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-30 10:09:29 +0100

    Let rooks move laterally

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,16 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+
+            for (var i = 0; i < 8; i++)
+            {
+                if (i != currentSquare.Col) availableMoves.Add(Square.At(currentSquare.Row, i));
+                if (i != currentSquare.Row) availableMoves.Add(Square.At(i, currentSquare.Col));
+            }
+
+            return availableMoves;
         }
     }
 }
