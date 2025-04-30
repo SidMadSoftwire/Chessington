94c2245a33a770bf26e550eb29db66e5ac265fb5
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-30 09:28:13 +0100

    Implement pawn moving functionality
    Pawns can move forward one square, capture forward one diagonal, and
    jump forward on the first move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -9,8 +9,34 @@ namespace Chessington.GameEngine.Pieces
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
-        {
-            return Enumerable.Empty<Square>();
+        {   
+            var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+
+            var dir = this.Player == Player.White ? -1 : 1;
+
+            if (currentSquare.Row + dir >= GameSettings.BoardSize || currentSquare.Row + dir < 0) return availableMoves;
+
+            var frontSquare = Square.At(currentSquare.Row + dir, currentSquare.Col);
+            if (board.GetPiece(frontSquare) == null)
+                availableMoves.Add(frontSquare);
+               
+            var diagonalLeftSquare = Square.At(currentSquare.Row + dir, currentSquare.Col - 1);
+            if (diagonalLeftSquare.Col >= 0 &&board.GetPiece(diagonalLeftSquare) != null)
+                availableMoves.Add(diagonalLeftSquare);
+               
+            var diagonalRightSquare = Square.At(currentSquare.Row + dir, currentSquare.Col + 1);
+            if (diagonalRightSquare.Col < GameSettings.BoardSize && board.GetPiece(diagonalRightSquare) != null)
+                availableMoves.Add(diagonalRightSquare);
+   
+            if ((dir == 1 && currentSquare.Row == 1) || (dir == -1 && currentSquare.Row == GameSettings.BoardSize - 2))
+            {
+                var doubleFrontSquare = Square.At(currentSquare.Row + 2 * dir, currentSquare.Col);
+                if (board.GetPiece(doubleFrontSquare) == null)
+                    availableMoves.Add(doubleFrontSquare);
+            }
+            
+            return availableMoves;
         }
     }
 }
