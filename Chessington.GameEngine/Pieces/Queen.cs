fb62ab674d25abfea50196b988a096622238e334
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-30 14:48:01 +0100

    Stop queen from moving through pieces

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -13,11 +13,17 @@ namespace Chessington.GameEngine.Pieces
             var availableMoves = new List<Square>();
             var currentSquare = board.FindPiece(this);
             const int boardSize = GameSettings.BoardSize;
+            
+            // Set possible directions for the queen to move.
             var directions = new[] {
                 (1, 1),
                 (-1, 1),
                 (1, -1),
-                (-1, -1) 
+                (-1, -1),
+                (0, 1),
+                (0, -1),
+                (1, 0),
+                (-1, 0) 
             };
             
             
@@ -28,15 +34,10 @@ namespace Chessington.GameEngine.Pieces
                     var newRow = currentSquare.Row + i * rowDir;
                     var newCol = currentSquare.Col + i * colDir;
                     
-                    if (newRow >= 0 && newRow < boardSize && newCol >= 0 && newCol < boardSize) 
-                        availableMoves.Add(Square.At(newRow, newCol));
+                    if (newRow < 0 || newRow >= boardSize || newCol < 0 || newCol >= boardSize) break;
+                    availableMoves.Add(Square.At(newRow, newCol));
+                    if (board.GetPiece(Square.At(newRow, newCol)) != null) break;
                 }
-            }
-            
-            for (var i = 0; i < 8; i++)
-            {
-                if (i != currentSquare.Col) availableMoves.Add(Square.At(currentSquare.Row, i));
-                if (i != currentSquare.Row) availableMoves.Add(Square.At(i, currentSquare.Col));
             }
 
             return availableMoves;
